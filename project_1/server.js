0af572acc1714e943c0ec52341ab0b39a93a9bad
drishti-gupta0704 <drishti.gupta070424@gmail.com>
2025-12-26 17:15:19 +0530

    adding project  with server, pages, and public assets

---
+++ project_1/server.js
@@ -0,0 +1,36 @@
+
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+
+const server = http.createServer((req, res) => {
+
+  if (req.url === "/style.css") {
+    const filePath = path.join(__dirname, "public4", "style.css");
+    const data = fs.readFileSync(filePath);
+    res.writeHead(200, { "Content-Type": "text/css" });
+    res.end(data);
+  }
+
+  else if (req.url === "/script.js") {
+    const filePath = path.join(__dirname, "public4", "script.js");
+    const data = fs.readFileSync(filePath);
+    res.writeHead(200, { "Content-Type": "text/javascript" });
+    res.end(data);
+  }
+
+  else if (req.url === "/") {
+    const filePath = path.join(__dirname, "pages4", "home.html");
+    const data = fs.readFileSync(filePath);
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end(data);
+  }
+  // 404 fallback handling 
+  else {
+    res.writeHead(404, { "Content-Type": "text/plain" });
+    res.end("404 Not Found");
+  }
+
+});
+
+server.listen(3000);
