07ae7d908c877dcbf2144e1f225b5dd39d1cc83c
drishti-gupta0704 <drishti.gupta070424@gmail.com>
2025-12-29 19:22:31 +0530

    adding manual routing logic through if-else

---
+++ multi_page_server/server.js
@@ -0,0 +1,24 @@
+
+
+
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+
+  if (req.url === "/") {
+    res.end("Welcome to Home Page");
+  } 
+  else if (req.url === "/about") {
+    res.end("This is About Page");
+  } 
+  else if (req.url === "/login") {
+    res.end("Login Page");
+  } 
+  else {
+    res.statusCode = 404;
+    res.end("404 Page Not Found");
+  }
+
+});
+
+server.listen(3000);
