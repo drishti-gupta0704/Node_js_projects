e729ff2be8fc99d9f7183867c0678b24ea29937d
drishti-gupta0704 <drishti.gupta070424@gmail.com>
2026-01-04 11:57:16 +0530

    adding logic to show JSON data

---
+++ return_JSON_data/server.js
@@ -0,0 +1,25 @@
+
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+
+  if (req.url === "/data" && req.method === "GET") {
+
+    const data = {
+      name: "Drishti",
+      role: "Student"
+    };
+
+    res.writeHead(200, {
+      "Content-Type": "application/json"
+    });
+
+    res.end(JSON.stringify(data));
+  }
+
+  
+});
+
+server.listen(3000, () => {
+  console.log("Server running on http://localhost:3000");
+});
